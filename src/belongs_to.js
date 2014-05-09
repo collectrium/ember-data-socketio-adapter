645eef2f3f702f8c4b6d108ffdf6b74d6a3f79c7
paloskin <paloskin@live.com>
2014-05-09 21:27:48 +0300

    Add new files.

---
+++ src/belongs_to.js
@@ -0,0 +1,164 @@
+var get = Ember.get, set = Ember.set,
+    isNone = Ember.isNone;
+
+var Promise = Ember.RSVP.Promise;
+
+
+/**
+  @module ember-data
+*/
+
+function asyncBelongsTo(type, options, meta) {
+  return Ember.computed('data', function(key, value) {
+    var data = get(this, 'data'),
+        store = get(this, 'store'),
+        promiseLabel = "DS: Async belongsTo " + this + " : " + key,
+        promise;
+
+    if (arguments.length === 2) {
+      Ember.assert("You can only add a '" + type + "' record to this relationship", !value || value instanceof store.modelFor(type));
+      return value === undefined ? null : DS.PromiseObject.create({
+        promise: Promise.cast(value, promiseLabel)
+      });
+    }
+
+    var link = data.links && data.links[key],
+        belongsTo = data[key];
+
+    if(!isNone(belongsTo)) {
+      promise = store.fetchRecord(belongsTo) || Promise.cast(belongsTo, promiseLabel);
+      return DS.PromiseObject.create({
+        promise: promise
+      });
+    } else if (link) {
+      promise = store.findBelongsTo(this, link, meta);
+      return DS.PromiseObject.create({
+        promise: promise
+      });
+    } else {
+      return null;
+    }
+  }).meta(meta);
+}
+
+/**
+  `DS.belongsTo` is used to define One-To-One and One-To-Many
+  relationships on a [DS.Model](/api/data/classes/DS.Model.html).
+
+
+  `DS.belongsTo` takes an optional hash as a second parameter, currently
+  supported options are:
+
+  - `async`: A boolean value used to explicitly declare this to be an async relationship.
+  - `inverse`: A string used to identify the inverse property on a
+    related model in a One-To-Many relationship. See [Explicit Inverses](#toc_explicit-inverses)
+
+  #### One-To-One
+  To declare a one-to-one relationship between two models, use
+  `DS.belongsTo`:
+
+  ```javascript
+  App.User = DS.Model.extend({
+    profile: DS.belongsTo('profile')
+  });
+
+  App.Profile = DS.Model.extend({
+    user: DS.belongsTo('user')
+  });
+  ```
+
+  #### One-To-Many
+  To declare a one-to-many relationship between two models, use
+  `DS.belongsTo` in combination with `DS.hasMany`, like this:
+
+  ```javascript
+  App.Post = DS.Model.extend({
+    comments: DS.hasMany('comment')
+  });
+
+  App.Comment = DS.Model.extend({
+    post: DS.belongsTo('post')
+  });
+  ```
+
+  @namespace
+  @method belongsTo
+  @for DS
+  @param {String or DS.Model} type the model type of the relationship
+  @param {Object} options a hash of options
+  @return {Ember.computed} relationship
+*/
+DS.belongsTo = function (type, options) {
+  if (typeof type === 'object') {
+    options = type;
+    type = undefined;
+  } else {
+    Ember.assert("The first argument DS.belongsTo must be a model type or string, like DS.belongsTo(App.Person)", !!type && (typeof type === 'string' || Model.detect(type)));
+  }
+
+  options = options || {};
+
+  var meta = {
+    type: type,
+    isRelationship: true,
+    options: options,
+    kind: 'belongsTo'
+  };
+
+  return asyncBelongsTo(type, options, meta);
+};
+
+/**
+  These observers observe all `belongsTo` relationships on the record. See
+  `relationships/ext` to see how these observers get their dependencies.
+
+  @class Model
+  @namespace DS
+*/
+DS.Model.reopen({
+
+  /**
+    @method belongsToWillChange
+    @private
+    @static
+    @param record
+    @param key
+  */
+  belongsToWillChange: Ember.beforeObserver(function(record, key) {
+    if (get(record, 'isLoaded')) {
+      var oldParent = get(record, key);
+
+      if (oldParent) {
+        var store = get(record, 'store'),
+            change = DS.RelationshipChange.createChange(record, oldParent, store, { key: key, kind: "belongsTo", changeType: "remove" });
+
+        change.sync();
+        this._changesToSync[key] = change;
+      }
+    }
+  }),
+
+  /**
+    @method belongsToDidChange
+    @private
+    @static
+    @param record
+    @param key
+  */
+  belongsToDidChange: Ember.immediateObserver(function(record, key) {
+    if (get(record, 'isLoaded')) {
+      var newParent = get(record, key);
+
+      if (newParent) {
+        var store = get(record, 'store'),
+            change = DS.RelationshipChange.createChange(record, newParent, store, { key: key, kind: "belongsTo", changeType: "add" });
+
+        change.sync();
+      }
+    }
+
+    delete this._changesToSync[key];
+  })
+});
+
+export default DS.belongsTo;
