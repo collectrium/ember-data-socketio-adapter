a8dec72d87f2747c042dab195630c13eb2826167
Timofey Dergachev <t@exeto.me>
2017-05-15 15:52:17 +0300

    Fix diff for PATCH method in socket serializer

--- addon/serializers/socket.js
+++ addon/serializers/socket.js
@@ -3,68 +3,94 @@ import Ember from 'ember';
 
 const {
   get,
-  keys,
   copy,
   String: { underscore },
   merge,
   compare,
   isArray
- } = Ember;
+} = Ember;
 
 const {
   RESTSerializer
 } = DS;
 
 export default RESTSerializer.extend({
-  extractFindQuery(store, type, payload) {
-    return this.extractArray(store, type, payload.payload);
-  },
-  buildDiff(hash, snapshot) {
-    hash = copy(hash);
-    const isNew = get(snapshot, 'isNew');
-    if (isNew) {
-      return hash;
-    }
-    const diff = {
-      id: hash.id
-    };
-    const relationships = snapshot._internalModel._relationships;
-    const initializedRelationshipsKeys = keys(relationships.initializedRelationships);
-    const relationshipsData = {};
-    initializedRelationshipsKeys.forEach((key) => {
-      const relationship = relationships.get(key);
-      if (relationship.hasData && relationship.hasLoaded && relationship.canonicalState) {
-        if (isArray(relationship.canonicalState)) {
-          relationshipsData[key] = relationship.canonicalState.map((internalModel) => {
-            return get(internalModel.getRecord(), 'id');
-          });
-        } else {
-          relationshipsData[key] = get(relationship.canonicalState.getRecord(), 'id');
-        }
-      } else {
-        relationshipsData[key] = null;
-      }
-    });
-    const attributesData = get(snapshot, 'data');
-    const possibleHash = merge(attributesData, relationshipsData);
-    keys(possibleHash).forEach((key) => {
-      if (this.isDiffer(hash[key], possibleHash[key]) && hash[key] !== undefined) {
-        // TODO: handle dates and all transforms processed data correctly
-        diff[key] = hash[key];
-      }
-    });
-
-    return diff;
-  },
   isDiffer(a, b) {
     return compare(a, b);
   },
+
+  getChangedRelations(snapshot) {
+    const changedRelations = {};
+    const record = snapshot.record.toJSON();
+
+    snapshot.eachRelationship((name, meta) => {
+      const relationships = snapshot._internalModel._relationships;
+      const relationship = relationships.get(name);
+      let oldValue = null;
+
+      if (relationship.hasData && relationship.hasLoaded && relationship.canonicalState) {
+        if (isArray(relationship.canonicalState)) {
+          oldValue = relationship.canonicalState.map((internalModel) => {
+            return get(internalModel.getRecord(), 'id');
+          });
+        } else {
+          oldValue = get(relationship.canonicalState.getRecord(), 'id');
+        }
+      }
+
+      const newValue = record[name];
+      changedRelations[name] = [oldValue, newValue];
+    });
+
+    return changedRelations;
+  },
+
+  getChangedAttributesWithRelations(snapshot) {
+    const changedRelations = this.getChangedRelations(snapshot);
+    const changedAttributes = snapshot.changedAttributes();
+    return merge(changedAttributes, changedRelations);
+  },
+
+  buildDiff(hash, snapshot) {
+    hash = copy(hash);
+    const isNew = get(snapshot, 'isNew');
+
+    if (isNew) {
+      return hash;
+    }
+
+    const changedAttributesWithRelations = this.getChangedAttributesWithRelations(snapshot);
+
+    return Object.keys(changedAttributesWithRelations).reduce((diff, key) => {
+      const [oldValue, newValue] = changedAttributesWithRelations[key];
+
+      if (this.isDiffer(oldValue, newValue)) {
+        diff[key] = newValue;
+      }
+
+      return diff;
+    }, { id: hash.id });
+  },
+
+  serialize(snapshot, options = {}) {
+    const { updateAsPatch } = options;
+    let hash = this._super(snapshot, options);
+
+    if (updateAsPatch) {
+      hash = this.buildDiff(hash, snapshot);
+    }
+
+    return hash;
+  },
+
   payloadKeyFromModelName(modelName) {
     return underscore(modelName);
   },
+
   serializeIntoHash(hash, typeClass, snapshot, options) {
     const isBulkOperation = snapshot instanceof Array;
     const normalizedRootKey = this.payloadKeyFromModelName(typeClass.modelName);
+
     if (isBulkOperation) {
       const bulkPayload = [];
       snapshot.forEach((snapshotData) => {
@@ -75,27 +101,28 @@ export default RESTSerializer.extend({
       hash[normalizedRootKey] = this.serialize(snapshot, options);
     }
   },
+
+  extractFindQuery(store, type, payload) {
+    return this.extractArray(store, type, payload.payload);
+  },
+
   extractFindAll(store, type, payload) {
     return this.extractArray(store, type, payload.payload);
   },
+
   extractFindMany(store, type, payload) {
     return this.extractArray(store, type, payload.payload);
   },
+
   extractCreateRecords(store, type, payload) {
     return this.extractArray(store, type, payload);
   },
+
   extractUpdateRecords(store, type, payload) {
     return this.extractArray(store, type, payload);
   },
+
   extractDeleteRecords(store, type, payload) {
     return this.extractArray(store, type, payload);
-  },
-  serialize(snapshot, options = {}) {
-    const { updateAsPatch } = options;
-    let hash = this._super(snapshot, options);
-    if (updateAsPatch) {
-      hash = this.buildDiff(hash, snapshot);
-    }
-    return hash;
   }
 });
